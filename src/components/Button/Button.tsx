de6a45a81460c6a9452b92b7c268f9d93b9609c5
Salifu Sani Rich <salifusanirich@gmail.com>
2022-08-12 14:01:17 +0100

    feat(components): Add Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,70 @@
+import { ButtonHTMLAttributes } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { Link, LinkProps } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+  variant?: 'inline' | 'base' | 'outline';
+}
+
+interface ButtonLinkProps extends LinkProps {
+  to: string;
+  isExternal?: boolean;
+  label: string;
+  variant?: 'inline' | 'base' | 'outline';
+}
+
+type Props = ButtonProps | ButtonLinkProps;
+
+function Button({ label, className, variant = 'base', ...props }: Props) {
+  if ('to' in props) {
+    const { isExternal, to, ...linkProps } = props;
+    if (isExternal) {
+      return (
+        <a
+          href={to}
+          className={cx('btn', 'btn-link', variant, className)}
+          target="_blank"
+          rel="noreferrer"
+          {...linkProps}
+        >
+          {label}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={cx('svg')}
+            data-testid="external-link-icon"
+          >
+            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+            <polyline points="15 3 21 3 21 9" />
+            <line x1="10" y1="14" x2="21" y2="3" />
+          </svg>
+        </a>
+      );
+    } else {
+      return (
+        <Link className={cx('btn', variant, className)} {...props}>
+          {label}
+        </Link>
+      );
+    }
+  } else {
+    return (
+      <button className={cx('btn', variant, className)} {...props}>
+        {label}
+      </button>
+    );
+  }
+}
+
+export default Button;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
+export { default as Button } from './Button/Button';
+export { default as Container } from './Container/Container';
 export { default as Logo } from './Logo/Logo';
 export { default as Welcome } from './Welcome/Welcome';
