b1400142674520692950a6dbe031c36aea419114
Salifu Sani Rich <salifusanirich@gmail.com>
2022-08-30 00:54:16 +0100

    feat(components): Add Layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+import classNames from 'classnames/bind';
+import { Container, Navbar } from '../';
+import styles from './Layout.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface LayoutProps {
+  main: ReactNode;
+}
+
+function Layout({ main }: LayoutProps) {
+  return (
+    <Container className={cx('layout')}>
+      <Navbar />
+      <div className={cx('main')} data-testid="layout-content">
+        {main}
+      </div>
+    </Container>
+  );
+}
+
+export default Layout;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Button } from './Button/Button';
 export { default as Checkbox } from './Checkbox/Checkbox';
 export { default as Container } from './Container/Container';
 export { default as Input } from './Input/Input';
+export { default as Layout } from './Layout/Layout';
 export { default as Loader } from './Loader/Loader';
 export { default as Logo } from './Logo/Logo';
 export { default as Tag } from './Tag/Tag';
