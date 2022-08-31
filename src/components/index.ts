d29c4ecf9276fbfe97bf11c1311c486019b8982d
Salifu Sani Rich <salifusanirich@gmail.com>
2022-08-31 07:21:57 +0100

    feat(components): Add Alert component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Alert } from './Alert/Alert';
 export { default as Avatar } from './Avatar/Avatar';
 export { default as Button } from './Button/Button';
 export { default as Checkbox } from './Checkbox/Checkbox';
